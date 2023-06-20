dfc80b8b46ed2e1f47eb9704b996ea0b0b2cece4
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-06-21 02:29:01 +0530

    creating itemdetails page item slider

--- lib/src/features/menu/screens/item_detials_page.dart
+++ lib/src/features/menu/screens/item_detials_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-class ItemDetailsPage extends StatelessWidget {
+import '../widgets/item_preview_slider.dart';
+
+class ItemDetailsPage extends StatefulWidget {
   const ItemDetailsPage({super.key});
 
   @override
+  State<ItemDetailsPage> createState() => _ItemDetailsPageState();
+}
+
+class _ItemDetailsPageState extends State<ItemDetailsPage> {
+ 
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          ItemPreviewSlider(),
+        ],
+      ),
+    );
   }
 }
