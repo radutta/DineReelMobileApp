40876b2d243cf3926566e6b780fd7bfcf608b23c
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-06-23 00:00:25 +0530

    making changes in colors file

--- lib/src/themes/colors.dart
+++ lib/src/themes/colors.dart
@@ -19,5 +19,5 @@ class AppColors {
   static const grey = Colors.grey;
   static const pinbgColor = Color(0XFFEFF3FF);
   static const scanbgColor = Color(0xFF1E1E1E);
-  static const primaryappbarColor = Colors.amber;
+  static const primaryappbarColor = Color(0XFFFFBB0C);
 }
