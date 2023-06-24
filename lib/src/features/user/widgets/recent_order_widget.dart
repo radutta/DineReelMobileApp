a8befaf046ed865f45fb1f1e851359a9a7d2f64c
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-06-24 23:15:04 +0530

    creating recent order details design

--- lib/src/features/user/widgets/recent_order_widget.dart
+++ lib/src/features/user/widgets/recent_order_widget.dart
@@ -1,5 +1,7 @@
+import 'package:dinereel/src/features/user/screens/recent_order_details.dart';
 import 'package:flutter/material.dart';
 
+import '../../../routing/routing_function.dart';
 import '../../../themes/colors.dart';
 
 class RecentOrderWidget extends StatelessWidget {
@@ -30,171 +32,177 @@ class RecentOrderWidget extends StatelessWidget {
             shrinkWrap: true,
             itemCount: 3,
             itemBuilder: (context, index) {
-              return Container(
-                width: 385,
-                height: 94,
-                padding: const EdgeInsets.all(18),
-                margin: const EdgeInsets.all(10),
-                decoration: ShapeDecoration(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context)
+                      .push(Routes().createRoute(const RecentOrderdetails()));
+                },
+                child: Container(
+                  width: 385,
+                  height: 94,
+                  padding: const EdgeInsets.all(18),
+                  margin: const EdgeInsets.all(10),
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    shadows: const [
+                      BoxShadow(
+                        color: AppColors.recentordercardBoxShadow,
+                        blurRadius: 15,
+                        offset: Offset(5, 5),
+                        spreadRadius: 0,
+                      )
+                    ],
                   ),
-                  shadows: const [
-                    BoxShadow(
-                      color: AppColors.recentordercardBoxShadow,
-                      blurRadius: 15,
-                      offset: Offset(5, 5),
-                      spreadRadius: 0,
-                    )
-                  ],
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 65,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 88,
-                            height: 65,
-                            padding: const EdgeInsets.only(right: 15),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  width: 400,
-                                  height: 49,
-                                  child: Center(
-                                    child: Stack(
-                                      children: [
-                                        Positioned(
-                                          left: 24,
-                                          top: 0,
-                                          child: Container(
-                                            width: 50.07,
-                                            height: 49,
-                                            decoration: ShapeDecoration(
-                                              image: const DecorationImage(
-                                                image: NetworkImage(
-                                                    "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"),
-                                                fit: BoxFit.fill,
-                                              ),
-                                              shape: RoundedRectangleBorder(
-                                                side: const BorderSide(
-                                                    width: 0.50,
-                                                    color: Colors.white),
-                                                borderRadius:
-                                                    BorderRadius.circular(50),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 65,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 88,
+                              height: 65,
+                              padding: const EdgeInsets.only(right: 15),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 400,
+                                    height: 49,
+                                    child: Center(
+                                      child: Stack(
+                                        children: [
+                                          Positioned(
+                                            left: 24,
+                                            top: 0,
+                                            child: Container(
+                                              width: 50.07,
+                                              height: 49,
+                                              decoration: ShapeDecoration(
+                                                image: const DecorationImage(
+                                                  image: NetworkImage(
+                                                      "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"),
+                                                  fit: BoxFit.fill,
+                                                ),
+                                                shape: RoundedRectangleBorder(
+                                                  side: const BorderSide(
+                                                      width: 0.50,
+                                                      color: Colors.white),
+                                                  borderRadius:
+                                                      BorderRadius.circular(50),
+                                                ),
                                               ),
                                             ),
                                           ),
-                                        ),
-                                        Positioned(
-                                          left: 16.69,
-                                          top: 0,
-                                          child: Container(
-                                            width: 50.07,
-                                            height: 49,
-                                            decoration: ShapeDecoration(
-                                              image: const DecorationImage(
-                                                image: NetworkImage(
-                                                    "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"),
-                                                fit: BoxFit.fill,
-                                              ),
-                                              shape: RoundedRectangleBorder(
-                                                side: const BorderSide(
-                                                    width: 0.50,
-                                                    color: Colors.white),
-                                                borderRadius:
-                                                    BorderRadius.circular(50),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                        Positioned(
-                                          left: 0,
-                                          top: 0,
-                                          child: Container(
-                                            width: 50.07,
-                                            height: 49,
-                                            decoration: ShapeDecoration(
-                                              image: const DecorationImage(
-                                                image: NetworkImage(
-                                                    "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTl8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"),
-                                                fit: BoxFit.fill,
-                                              ),
-                                              shape: RoundedRectangleBorder(
-                                                side: const BorderSide(
-                                                    width: 0.50,
-                                                    color: Colors.white),
-                                                borderRadius:
-                                                    BorderRadius.circular(50),
+                                          Positioned(
+                                            left: 16.69,
+                                            top: 0,
+                                            child: Container(
+                                              width: 50.07,
+                                              height: 49,
+                                              decoration: ShapeDecoration(
+                                                image: const DecorationImage(
+                                                  image: NetworkImage(
+                                                      "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTJ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"),
+                                                  fit: BoxFit.fill,
+                                                ),
+                                                shape: RoundedRectangleBorder(
+                                                  side: const BorderSide(
+                                                      width: 0.50,
+                                                      color: Colors.white),
+                                                  borderRadius:
+                                                      BorderRadius.circular(50),
+                                                ),
                                               ),
                                             ),
                                           ),
-                                        ),
-                                      ],
+                                          Positioned(
+                                            left: 0,
+                                            top: 0,
+                                            child: Container(
+                                              width: 50.07,
+                                              height: 49,
+                                              decoration: ShapeDecoration(
+                                                image: const DecorationImage(
+                                                  image: NetworkImage(
+                                                      "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTl8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"),
+                                                  fit: BoxFit.fill,
+                                                ),
+                                                shape: RoundedRectangleBorder(
+                                                  side: const BorderSide(
+                                                      width: 0.50,
+                                                      color: Colors.white),
+                                                  borderRadius:
+                                                      BorderRadius.circular(50),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                          Text('Biriyani, Chicken \nChap, Lassi,+3\nMore',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodySmall!
-                                  .copyWith(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 12)),
-                          const SizedBox(width: 85),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('29 May, 23',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodySmall!
-                                      .copyWith(fontSize: 10)),
-                              Container(
-                                width: 75,
-                                height: 40,
-                                decoration: ShapeDecoration(
-                                  gradient: const LinearGradient(
-                                    begin: Alignment(1.00, 0.00),
-                                    end: Alignment(-1, 0),
-                                    colors: [
-                                      Color(0xFFFFA000),
-                                      Color(0xFFFFBB0C)
-                                    ],
-                                  ),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                                alignment: Alignment.center,
-                                child: Text('₹ 1340',
-                                    textAlign: TextAlign.center,
+                            Text('Biriyani, Chicken \nChap, Lassi,+3\nMore',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall!
+                                    .copyWith(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 12)),
+                            const SizedBox(width: 85),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('29 May, 23',
                                     style: Theme.of(context)
                                         .textTheme
-                                        .bodyMedium!
-                                        .copyWith(fontSize: 16)),
-                              )
-                            ],
-                          )
-                        ],
+                                        .bodySmall!
+                                        .copyWith(fontSize: 10)),
+                                Container(
+                                  width: 75,
+                                  height: 40,
+                                  decoration: ShapeDecoration(
+                                    gradient: const LinearGradient(
+                                      begin: Alignment(1.00, 0.00),
+                                      end: Alignment(-1, 0),
+                                      colors: [
+                                        Color(0xFFFFA000),
+                                        Color(0xFFFFBB0C)
+                                      ],
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Text('₹ 1340',
+                                      textAlign: TextAlign.center,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium!
+                                          .copyWith(fontSize: 16)),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             })
