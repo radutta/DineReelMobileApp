14bcffdc6be59e6f298c3289fd148c469ff59210
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2023-08-02 02:18:18 +0530

    making feature to go to menu screen after scaning

--- lib/src/features/menu/screens/qr_scan_page.dart
+++ lib/src/features/menu/screens/qr_scan_page.dart
@@ -1,7 +1,10 @@
+import 'dart:developer';
 import 'dart:io';
+
 import 'package:dinereel/src/features/menu/screens/menu_page.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import '../../../themes/colors.dart';
@@ -19,8 +22,12 @@ class _QrScanPageState extends State<QrScanPage> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
+  late PermissionStatus status;
+  bool scaning = true;
   @override
   void initState() {
+    // getCameraPermission();
+    // getStatus();
     super.initState();
   }
 
@@ -36,6 +43,43 @@ class _QrScanPageState extends State<QrScanPage> {
     }
   }
 
+  Future<PermissionStatus> getCameraPermission() async {
+    var status = await Permission.camera.status;
+    if (!status.isGranted) {
+      final result = await Permission.camera.request();
+      return result;
+    } else {
+      return status;
+    }
+  }
+
+  Future<bool> checkPermissionStatus() async {
+    const permission = Permission.camera;
+    print(permission);
+    return await permission.status.isGranted;
+  }
+
+  getStatus() async {
+    status = await getCameraPermission();
+    print('status-->$status');
+  }
+
+  Future<void> requestPermission() async {
+    const permission = Permission.camera;
+
+    if (await permission.isDenied) {
+      final result = await permission.request();
+
+      if (result.isGranted) {
+        // Permission is granted
+      } else if (result.isDenied) {
+        // Permission is denied
+      } else if (result.isPermanentlyDenied) {
+        // Permission is permanently denied
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,23 +92,33 @@ class _QrScanPageState extends State<QrScanPage> {
             children: [
               const SizedBox(height: 50),
               GestureDetector(
-                onTap: () async {
-                  result != null
-                      ? Navigator.of(context)
-                          .push(Routes().createRoute(const MenuHome()))
-                      : Navigator.of(context)
-                          .push(Routes().createRoute(const Home()));
+                onTap: () {
+                  Navigator.of(context)
+                      .push(Routes().createRoute(const Home()));
                 },
                 child: const Icon(Icons.arrow_back, color: AppColors.white),
               ),
               const SizedBox(height: 50),
-              Center(
-                  child: Text('scan_qr'.tr(),
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleLarge!
-                          .copyWith(color: AppColors.white))),
+              GestureDetector(
+                onTap: () {
+                  getCameraPermission();
+                  getStatus();
+                },
+                child: Center(
+                    child: Text('scan_qr'.tr(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleLarge!
+                            .copyWith(color: AppColors.white))),
+              ),
               const SizedBox(height: 52),
+              Center(
+                child: Text(
+                  result.toString(),
+                  style: TextStyle(color: AppColors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
               Center(
                   child: SizedBox(
                       height: 300,
@@ -89,16 +143,27 @@ class _QrScanPageState extends State<QrScanPage> {
 
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
+    controller.resumeCamera();
     controller.scannedDataStream.listen((scanData) {
+      log(scanData.code!);
       setState(() {
         result = scanData;
       });
+      if (scaning) {
+        scaning = false;
+        controller.pauseCamera();
+        scanData.toString().isNotEmpty
+            ? Navigator.of(context).push(Routes().createRoute(const MenuHome()))
+            : Navigator.of(context).push(Routes().createRoute(const Home()));
+      }
     });
+
+    this.controller!.pauseCamera();
+    this.controller!.resumeCamera();
   }
 
   @override
   void dispose() {
-    controller?.dispose();
     super.dispose();
   }
 }
